6d4895a08880450e495ebc8e68332f15951cb5f4
benyemna <mohamed.ben-yemna@inria.fr>
2014-08-21 10:40:34 +0200

    Generator

--- js/codegenerator.js
+++ js/codegenerator.js
@@ -5,3 +5,57 @@
  */
 
 
+var CodeGenerator;
+
+CodeGenerator.prototype.getBlocks = function (XML){
+    var code = [];
+  this.init();
+  var blocks = Workspace.getTopBlocks(true);
+  for (var x = 0, block; block = blocks[x]; x++) {
+    var line = this.blockToCode(block);
+    if (isArray(line)) {
+      line = line[0];
+    }
+    if (line) {
+      if (block.outputConnection && this.scrubNakedValue) {
+        line = this.scrubNakedValue(line);
+      }
+      code.push(line);
+    }
+  }
+  code = code.join('\n');  // Blank line between each section.
+  code = this.finish(code);
+  code = code.replace(/^\s+\n/, '');
+  code = code.replace(/\n\s+$/, '\n');
+  code = code.replace(/[ \t]+\n/g, '\n');
+  return code;
+};
+
+CodeGenerator.prototype.blockToCode = function(block) {
+  if (!block) {
+    return '';
+  }
+  if (block.disabled) {
+    return this.blockToCode(block.getNextBlock());
+  }
+
+  var func = this[block.type];
+  if (!func) {
+    throw 'Language "' + this.name_ + '" does not know how to generate code ' +
+        'for block type "' + block.type + '".';
+  }
+  var code = func.call(block, block);
+  if (isArray(code)) {
+     return [this.scrub_(block, code[0]), code[1]];
+  } else if (goog.isString(code)) {
+    if (this.STATEMENT_PREFIX) {
+      code = this.STATEMENT_PREFIX.replace(/%1/g, '\'' + block.id + '\'') +
+          code;
+    }
+    return this.scrub_(block, code);
+  } else if (code === null) {
+    return '';
+  } else {
+    throw 'Invalid code generated: ' + code;
+  }
+};
